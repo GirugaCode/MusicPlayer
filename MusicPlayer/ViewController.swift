42fc6b2727c67a5068d3c2937a98b4d4f40f6d07
GirugaCode <ryan.dphu.nguyen@gmail.com>
2018-08-26 19:51:29 -0700

    Initial Commit

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Ryan Nguyen on 8/26/18.
+//  Copyright © 2018 Danh Phu Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
